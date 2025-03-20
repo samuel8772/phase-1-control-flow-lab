069a4a417a0a26dd80cc15d9e3187ca74d9566f3
samuel8772 <kamauskk005@gmail.com>
2025-03-20 10:57:52 +0300

    complete lab

--- index.js
+++ index.js
@@ -8,4 +8,34 @@ function ternaryCheckCity(){
 
 function switchOnCharmFromTip(){
   // Write your code here!
+}// helpers.js
+
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 400 && distance <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
+
+// Export the functions
+module.exports = { scuberGreetingForFeet, ternaryCheckCity, switchOnCharmFromTip };
